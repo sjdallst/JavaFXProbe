87f0d094b4e8aa43e4e6e6f183c79c80e402ea97
Samuel Dallstream <sjdallst@gmail.com>
2014-06-04 12:01:10 -0400

    Added counter, displays total number of clicks. All styling is default.

--- src/javafxprobe/JavaFxProbe.java
+++ src/javafxprobe/JavaFxProbe.java
@@ -7,34 +7,58 @@ package javafxprobe;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
 
 /**
  *
- * @author smaceul
+ * @author sjdallst
  */
 public class JavaFxProbe extends Application {
     
+    private int count= 0;
+    private Button btn = new Button();
+    private Text countText = new Text("" + count);
+    
     @Override
     public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
+
+        btn.setText("+1");
         btn.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
+                increment();
             }
         });
         
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
+        HBox hbBtn = new HBox(10);
+        hbBtn.setAlignment(Pos.CENTER);
+        hbBtn.getChildren().add(btn);
         
-        Scene scene = new Scene(root, 300, 250);
+        HBox hbText = new HBox(10);
+        hbText.setAlignment(Pos.CENTER);
+        hbText.getChildren().add(countText);
+      
+        GridPane grid = new GridPane();
+        grid.setAlignment(Pos.CENTER);
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(25, 25, 25, 25));
+        Scene scene = new Scene(grid, 300, 250);
         
-        primaryStage.setTitle("Hello World!");
+        grid.add(hbText, 0, 0);
+        grid.add(hbBtn, 0, 1);
+        
+        //grid.setGridLinesVisible(true);
+        
+        primaryStage.setTitle("Probe");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
@@ -49,5 +73,10 @@ public class JavaFxProbe extends Application {
      */
     public static void main(String[] args) {
         launch(args);
+    }
+    
+    public void increment(){
+        count++;
+        countText.setText("" + count);
     }
 }
