43a191cff005363d05aabe02652be31d30e32709
Samuel Dallstream <sjdallst@gmail.com>
2014-06-04 16:57:04 -0400

    Added TextField for text input and TableView

--- src/javafxprobe/JavaFxProbe.java
+++ src/javafxprobe/JavaFxProbe.java
@@ -5,16 +5,23 @@
 package javafxprobe;
 
 import javafx.application.Application;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
-import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
 
 /**
@@ -26,6 +33,11 @@ public class JavaFxProbe extends Application {
     private int count= 0;
     private Button btn = new Button();
     private Text countText = new Text("" + count);
+    private Text textFieldLabel = new Text("Stuff: ");
+    private TextField textField = new TextField();
+    private Text textFieldInput = new Text("");
+    private final TableView<Thing> table = new TableView<>();
+    private ObservableList<Thing> data = FXCollections.observableArrayList(new Thing(), new Thing());
     
     @Override
     public void start(Stage primaryStage) {
@@ -45,16 +57,66 @@ public class JavaFxProbe extends Application {
         HBox hbText = new HBox(10);
         hbText.setAlignment(Pos.CENTER);
         hbText.getChildren().add(countText);
+        
+        textField.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                textFieldInput.setText(textField.getText());
+                textField.clear();
+            }
+        });
+        
+        HBox hbInputText = new HBox(10);
+        hbInputText.setAlignment(Pos.CENTER_LEFT);
+        hbInputText.getChildren().addAll(textFieldLabel, textField, textFieldInput);
+        
+        TableColumn firstColumn = new TableColumn("First");
+        firstColumn.setMinWidth(100);
+        firstColumn.setCellValueFactory(
+                new PropertyValueFactory<>("first"));
+        
+        TableColumn secondColumn = new TableColumn("Second");
+        secondColumn.setMinWidth(100);
+        secondColumn.setCellValueFactory(
+                new PropertyValueFactory<>("second"));
+        
+        TableColumn thirdColumn = new TableColumn("Third");
+        thirdColumn.setMinWidth(100);
+        thirdColumn.setCellValueFactory(
+                new PropertyValueFactory<>("third"));
+        
+        table.setItems(data);
+        table.getColumns().addAll(firstColumn, secondColumn, thirdColumn);
+        table.setMaxWidth(600);
+        table.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
+        
+        Button addButton = new Button("Add something");
+        addButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                data.add(new Thing());
+            }
+        });
+        
+        VBox vBox = new VBox(10);
+        vBox.setAlignment(Pos.CENTER);
+        vBox.setSpacing(5);
+        vBox.setPadding(new Insets(10, 0, 0, 10));
+        vBox.setMaxWidth(600);
+        vBox.getChildren().addAll(addButton, table);
       
         GridPane grid = new GridPane();
         grid.setAlignment(Pos.CENTER);
         grid.setHgap(10);
         grid.setVgap(10);
         grid.setPadding(new Insets(25, 25, 25, 25));
-        Scene scene = new Scene(grid, 300, 250);
+        Scene scene = new Scene(grid, 800, 250);
         
         grid.add(hbText, 0, 0);
         grid.add(hbBtn, 0, 1);
+        grid.add(hbInputText, 0, 2);
+        grid.add(vBox,0,3);
+        grid.setMaxWidth(600);
         
         //grid.setGridLinesVisible(true);
         
@@ -78,5 +140,41 @@ public class JavaFxProbe extends Application {
     public void increment(){
         count++;
         countText.setText("" + count);
+    }
+    
+    public static class Thing{
+        
+        private final SimpleDoubleProperty first, second, third;
+        
+        public Thing(){
+            first = new SimpleDoubleProperty(Math.random());
+            second = new SimpleDoubleProperty(Math.random());
+            third = new SimpleDoubleProperty(Math.random());
+        }
+        
+        public void setFirst(double f){
+            first.set(f);
+        }
+        
+        public void setSecond(double s){
+            second.set(s);
+        }
+        
+        public void setThird(double t){
+            third.set(t);
+        }
+        
+        public double getFirst(){
+            return first.get();
+        }
+        
+        public double getSecond(){
+            return second.get();
+        }
+        
+        public double getThird(){
+            return third.get();
+        }
+        
     }
 }
